7c24ab7eef895b1a46005e799a1667c364ac8d29
IsraelBoka <108950893+IsraelBoka@users.noreply.github.com>
2023-01-28 15:40:53 +0100

    some changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,73 +1,64 @@
-import Link from "next/link";
-import { Button } from "./Button";
-import { Container } from "./container";
-import { signIn, signOut, useSession } from "next-auth/react";
-import { Logo } from "./icons/logo";
-import { Hamburger } from "./icons/hamburger";
-import classNames from "classnames";
-import { useEffect, useState } from "react";
+import Link from 'next/link';
+import { Button } from './Button';
+import { Container } from './container';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import { Logo } from './icons/logo';
+import { Hamburger } from './icons/hamburger';
+import classNames from 'classnames';
+import { useEffect, useState } from 'react';
 
 const Navbar = () => {
   const { data: sessionData } = useSession();
   const [open, setOpen] = useState(false);
-  useEffect (() => {
-    const html = document.querySelector("html");
-    if (html) html.classList.toggle("overflow-hidden", open);
-  } , [open])
+  useEffect(() => {
+    const html = document.querySelector('html');
+    if (html) html.classList.toggle('overflow-hidden', open);
+  }, [open]);
 
-  useEffect (() => {
-    window.addEventListener("orentationchange", () => { 
+  useEffect(() => {
+    window.addEventListener('orentationchange', () => {
       setOpen(false);
-    })
-    window.addEventListener("resize", () => {
+    });
+    window.addEventListener('resize', () => {
       setOpen(false);
-    })
+    });
 
     return () => {
-      window.removeEventListener("orentationchange", () => { 
+      window.removeEventListener('orentationchange', () => {
         setOpen(false);
-      })
-      window.removeEventListener("resize", () => {
+      });
+      window.removeEventListener('resize', () => {
         setOpen(false);
-      })
-    }
-
-  } , [open])
+      });
+    };
+  }, [open]);
   return (
-    <header className="z-10 fixed top-0 left-0 w-full px-8  backdrop-blur-[12px]">
+    <header className="fixed top-0 left-0 z-10 w-full px-8  backdrop-blur-[12px]">
       <Container classname="flex  h-12 justify-center items-center border-gray-700   border-b">
-        <Link
-          className="md:text-md mr-2 flex items-center justify-center"
-          href="#"
-        >
+        <Link className="md:text-md mr-2 flex items-center justify-center" href="#">
           <Logo classname="w-8 h-8 mr-2 stroke-white" />
           <p className="text-md font-extrabold">
             <span className="">DAB</span>
             <span className="text-blue-300">ARA</span>
           </p>
         </Link>
-        <div
-          className={classNames(
-            "md:visible ",
-            open ? "visible " : "invisible  delay-500"
-          )}
-        >
+        <div className={classNames('md:visible ', open ? 'visible ' : 'invisible  delay-500')}>
           <nav
             className={classNames(
-              "fixed top-12  left-0 h-[calc(100vh_-_3rem)] w-full overflow-auto bg-[#121216] transition-opacity duration-500 md:relative md:top-0 md:block md:h-auto md:w-auto  md:translate-x-0 md:bg-transparent md:opacity-100",
-              open
-                ? " opacity-100 "
-                : " opacity-0 translate-x-[-100vw] "
+              'fixed top-12  left-0 h-[calc(100vh_-_3rem)] w-full overflow-auto bg-[#121216] transition-opacity duration-500 md:relative md:top-0 md:block md:h-auto md:w-auto  md:translate-x-0 md:bg-transparent md:opacity-100',
+              open ? ' opacity-100 ' : ' translate-x-[-100vw] opacity-0 ',
             )}
           >
             <ul
               className={classNames(
-                "[&_a]:transition-[color, transform] [&_a]:mr-8 [&_a]:ml-1 stext-md flex  h-full flex-col ease-in md:flex-row md:items-center md:text-sm  [&_a]:flex [&_a]:h-12 [&_a]:translate-y-8 [&_a]:items-center [&_a]:border-b [&_a]:border-gray-500 [&_a]:duration-300 md:[&_a]:translate-y-0 md:[&_a]:border-b-0  [&_a:hover]:text-gray-300 [&_li]:ml-6",
-                open && "[&_a]:translate-y-0 "
+                '[&_a]:transition-[color, transform] stext-md flex h-full flex-col tracking-wide  ease-in md:flex-row md:items-center md:text-sm [&_a]:mr-8 [&_a]:ml-1  [&_a]:flex [&_a]:h-12 [&_a]:translate-y-8 [&_a]:items-center [&_a]:border-b [&_a]:border-gray-500 [&_a]:duration-300 md:[&_a]:translate-y-0 md:[&_a]:border-b-0  [&_a:hover]:text-gray-300 [&_li]:ml-6',
+                open && '[&_a]:translate-y-0 ',
               )}
             >
               <li>
-                <Link target={"_self"} href="/creerpage">Création</Link>
+                <Link target={'_self'} href="/creerpage">
+                  Création
+                </Link>
               </li>
               <li className="hidden">
                 <Link href="#">Sponsor</Link>
@@ -81,11 +72,11 @@ const Navbar = () => {
         </div>
         <div className="ml-auto flex h-full items-center  text-sm ">
           {sessionData?.user ? (
-            <Button >
+            <Button>
               <span onClick={() => void signOut()}>Déconnexion</span>
             </Button>
           ) : (
-            <Button > 
+            <Button>
               <span onClick={() => void signIn()}>Connexion</span>
             </Button>
           )}
